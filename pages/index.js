f6ff08f857c96711c0043c0f158c89c3f973e52f
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-03-04 14:02:34 +0200

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Central Creative Developer</title>
+          <meta property="og:title" content="Central Creative Developer" />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
---
+++ pages/test-page.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Central Creative Developer</title>
+          <meta
+            property="og:title"
+            content="test-page - Central Creative Developer"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_k9yzdo) => (
+            <>
+              <h1 id={context_k9yzdo?.id}>Heading</h1>
+            </>
+          )}
+          params={{
+            projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+            query:
+              'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+            attribute: 'id',
+            id: '2',
+          }}
+          initialData={props.contextK9yzdoProp}
+          persistDataDuringLoading={true}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const contextK9yzdoProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    if (!contextK9yzdoProp?.data?.[0]) {
+      return {
+        notFound: true,
+      }
+    }
+    return {
+      props: {
+        contextK9yzdoProp: contextK9yzdoProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
